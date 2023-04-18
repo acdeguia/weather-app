f420f16f24e65f52ec35760f24cdb8f280df0e8e
Ann Catherine <catherine.acee@gmail.com>
2023-04-18 19:55:34 +0800

    Fix getWeatherData function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import "../src/style.css";
 import weather from "./modules/api";
-// import search from "./modules/search";
 
-const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementById("search-location");
-const searchBtn = document.getElementById("search-btn");
 
-// searchForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// });
+const searchForm = document.querySelector('#search-form');
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent the form from submitting normally
+  });
 
-// searchBtn.addEventListener("click", async () => {
-//   if (searchInput.value === "") return;
-//   const weatherData = await weather.getData(searchInput.value);
-//   search.setSearchResult(weatherData);
-//   console.log(searchInput.value)
-// });
-
-weather()
+  weather()
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,12 +1,20 @@
 const weather = function () {
 
+
+
   async function getWeatherData(city) {
-    const response = await fetch( `https://api.weatherapi.com/v1/current.json?key=efc019cca96741a3aee74048231004&q=manila}`);
+    city = document.getElementById('search-location')
+    console.log(city)
+
+    try {
+      const response = await fetch( `https://api.weatherapi.com/v1/current.json?key=efc019cca96741a3aee74048231004&q=${city}}`);
     const data = await response.json();
     const processedData = processWeatherData(data);
     console.log(processedData); // log the processed weather data to the console
+    } catch (error) {
+      console.log(error);
+    }
   }
-
 
   function processWeatherData(data) {
     const processedData = {
@@ -14,6 +22,8 @@ const weather = function () {
       condition: data.current.condition.text,
       temp: data.current.feelslike_c,
       humidity: data.current.humidity,
+      wind: data.current.wind_kph,
+      chanceOfRain: data.current.precip_in,
     };
     return processedData;
   }
