fbbfa857a6ae37574bef464b0babf5a7b3c945e0
Ann Catherine <catherine.acee@gmail.com>
2023-04-18 21:03:25 +0800

    Fix styling

--- src/modules/api.js
+++ src/modules/api.js
@@ -5,6 +5,7 @@ const weather = (function () {
         `https://api.weatherapi.com/v1/current.json?key=efc019cca96741a3aee74048231004&q=${city}}`
       );
       const data = await response.json();
+      console.log(data)
       const processedData = processWeatherData(data);
       //console.log(processedData); // log the processed weather data to the console
       return processedData
@@ -18,6 +19,7 @@ const weather = (function () {
       city: data.location.name,
       condition: data.current.condition.text,
       temp: data.current.feelslike_c,
+      feelsLike: data.current.feelslike_c,
       humidity: data.current.humidity,
       wind: data.current.wind_kph,
       chanceOfRain: data.current.precip_in,
